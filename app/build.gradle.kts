45f98427caf9720f4e9ec8d440fd0148909ad51c
DrabornEagle <draborneagle@gmail.com>
2026-09-18 00:45:07 +0300

    build: compile against API 37 for current Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.draborneagle.drabornportal"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.draborneagle.drabornportal"
